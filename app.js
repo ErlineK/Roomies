20448808013c65f1da2ceee703f88e7db61347dc
Erline Katz <erline.kl@gmail.com>
2020-06-02 17:46:02 -0400

    pre deploy

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Join client
-app.use(express.static(path.join(__dirname, "/client/build")));
+// // Join client
+// app.use(express.static(path.join(__dirname, "/client/build")));
 
 // Use routes
 app.use("/api/users", require("./routes/api/users"));
@@ -32,9 +32,19 @@ app.use("/api/houses", require("./routes/api/houses"));
 app.use("/api/notifications", require("./routes/api/notifications"));
 app.use("/api/bills", require("./routes/api/bills"));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/client/build/index.html"));
+// });
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
