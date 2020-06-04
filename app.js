0ebd07625e8567cf4cd1b77684ad336c79fb4d70
Erline Katz <erline.kl@gmail.com>
2020-06-04 00:42:22 -0400

    app.js change

--- app.js
+++ app.js
@@ -36,11 +36,11 @@ app.use("/api/bills", require("./routes/api/bills"));
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("./client/build"));
+  app.use(express.static("client/build"));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "client/build/index.html"));
 });
 
 const port = process.env.PORT || 4000;
