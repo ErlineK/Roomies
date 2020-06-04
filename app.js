92c39f70b834dccb45450bd2340352d9f37b3ad8
Erline Katz <erline.kl@gmail.com>
2020-06-03 23:49:28 -0400

    port update

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
